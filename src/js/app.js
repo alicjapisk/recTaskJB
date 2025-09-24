f8705acd9ab421af845eb8fb4d83bacb9174dbe7
alicjapisk <alicjapisk@gmail.com>
2025-09-24 11:08:32 +0200

    add header of section

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import initInlineSVG from "./inline-svg.js";
+import { renderTeamCard } from "./team-card.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initInlineSVG();
@@ -12,152 +13,24 @@ async function getTeams() {
   return data.table;
 }
 
-function renderTeamCard(team) {
-  const template = document.getElementById("team-card-template");
-  const clone = template.content.cloneNode(true);
-  const score = countScore(+team.intWin, +team.intDraw, +team.intLoss);
-  const goalsConfig = [
-    {
-      sel: ".team-card__stats-details-goals-for",
-      val: team.intGoalsFor,
-      label: "Goals for",
-    },
-    {
-      sel: ".team-card__stats-details-goals-against",
-      val: team.intGoalsAgainst,
-      label: "Goals against",
-    },
-    {
-      sel: ".team-card__stats-details-goals-diff",
-      val: team.intGoalDifference,
-      label: "Goals difference",
-    },
-  ];
+let allTeams = [];
 
-  clone.querySelector(".team-card__badge").textContent = team.intRank;
-
-  if (+team.intRank <= 3) {
-    clone
-      .querySelector(".team-card__badge")
-      .style.setProperty(
-        "--team-card__badge-bg-color",
-        "var(--color-badge-default-top)"
-      );
-    clone
-      .querySelector(".team-card__badge")
-      .style.setProperty(
-        "--team-card__badge-color",
-        "var(--color-badge-foreground)"
-      );
-  } else {
-    clone
-      .querySelector(".team-card__badge")
-      .style.setProperty(
-        "--team-card__badge-bg-color",
-        "var(--color-badge-default)"
-      );
-    clone
-      .querySelector(".team-card__badge")
-      .style.setProperty(
-        "--team-card__badge-color",
-        "var(--color-badge-default-foreground)"
-      );
-  }
-
-  clone.querySelector(".team-card__image").src = team.strBadge;
-  clone.querySelector(".team-card__image").alt = team.strTeam;
-  clone.querySelector(".team-card__team-name").textContent = team.strTeam;
-  clone
-    .querySelector(".team-card__chart-wins")
-    .style.setProperty("--progress-wins", score.winsPercentage + "%");
-  clone
-    .querySelector(".team-card__chart-draws")
-    .style.setProperty("--progress-draws", score.drawsPercentage + "%");
-  clone
-    .querySelector(".team-card__chart-losses")
-    .style.setProperty("--progress-losses", score.lossesPercentage + "%");
-  clone.querySelector(
-    ".team-card__stats-wins"
-  ).textContent = `W: ${team.intWin}`;
-  clone.querySelector(
-    ".team-card__stats-draws"
-  ).textContent = `D: ${team.intDraw}`;
-  clone.querySelector(
-    ".team-card__stats-losses"
-  ).textContent = `L: ${team.intLoss}`;
-  clone.querySelector(".team-card__points-value").textContent =
-    team.intPoints + " PTS";
-  clone
-    .querySelector(".team-card__stats-details-form")
-    .replaceWith(renderForm(team.strForm));
-
-  goalsConfig.forEach(({ sel, val, label }) => {
-    clone.querySelector(sel)?.replaceWith(renderGoals(val, label));
-  });
-
-  return clone;
-}
-
-async function renderTeams() {
+async function renderTeams(filter = "") {
   const container = document.getElementById("teams_container");
-  const teams = await getTeams();
-  console.log(teams);
-  teams.forEach((team) => {
-    container.appendChild(renderTeamCard(team));
-  });
-}
+  container.innerHTML = "";
 
-function countScore(wins, draws, losses) {
-  const teamScoreSum = wins + draws + losses;
-  const winsPercentage = (wins / teamScoreSum) * 100;
-  const drawsPercentage = (draws / teamScoreSum) * 100;
-  const lossesPercentage = (losses / teamScoreSum) * 100;
-
-  return {
-    winsPercentage,
-    drawsPercentage,
-    lossesPercentage,
-    teamScoreSum,
-  };
-}
-
-function renderForm(form) {
-  const container = document.createElement("div");
-  container.classList.add("team-card__stats-details-form");
-
-  const label = document.createElement("span");
-  label.classList.add("team-card__stats-details-form-label");
-  label.textContent = "Form:";
-  container.appendChild(label);
-
-  const items = document.createElement("div");
-  items.classList.add("team-card__stats-details-form-items");
-
-  form
-    .split("")
-    .reverse()
-    .forEach((value) => {
-      const span = document.createElement("span");
-      span.classList.add(
-        "team-card__stats-details-form-items",
-        `team-card__stats-details-form-items-${value}`
-      );
-      span.textContent = value;
-      items.appendChild(span);
+  if (allTeams.length === 0) {
+    allTeams = await getTeams();
+  }
+  allTeams
+    .filter((team) => team.strTeam.toLowerCase().includes(filter.toLowerCase()))
+    .forEach((team) => {
+      container.appendChild(renderTeamCard(team));
     });
-
-  container.appendChild(items);
-  return container;
 }
 
-function renderGoals(value, text) {
-  const container = document.createElement("div");
-  container.classList.add("team-card__stats-details-goals");
-  container.innerHTML = `
-    <span class="team-card__stats-details-goals-label">${text}:</span>
-    <p class="team-card__stats-details-goals-value">${value}</p>
-  `;
-  return container;
-}
+document
+  .getElementById("searchInput")
+  .addEventListener("input", (e) => renderTeams(e.target.value));
 
 renderTeams();
